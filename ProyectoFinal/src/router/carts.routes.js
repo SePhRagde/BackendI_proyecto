92b90f12d6915f4e93c7c42ddcc36ef0bcd296e2
SePhRagde <SePh-Ragde@outlook.com>
2024-12-24 11:20:06 -0600

    Preliminar proyecto final

---
+++ ProyectoFinal/src/router/carts.routes.js
@@ -0,0 +1,40 @@
+import { Router } from "express";
+import { CartsManager } from "../managers/cartsManager.js";
+
+const cartsManager = new CartsManager();
+const router = Router();
+
+router.post("/", async (req, res) => {
+    try {
+        const newCart = await cartsManager.createCart();
+        res.status(201).send(newCart);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+router.get("/:cid", async (req, res) => {
+    const { cid } = req.params;
+    try {
+        const cart = await cartsManager.getCartById(cid);
+        res.send(cart);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+
+router.post("/:cid/product/:pid", async (req, res) => {
+    const { cid, pid } = req.params;
+    try {
+        const updatedCart = await cartsManager.addProductToCart(cid, pid);
+        res.send(updatedCart);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send({ message: error.message });
+    }
+});
+
+export default router;
